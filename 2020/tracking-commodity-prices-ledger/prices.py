88841357614b52fccd6347d7f541866380d98025
Lukas Joswiak <lukas@lukasjoswiak.com>
2020-08-29 22:07:30 -0700

    Update API source

--- 2020/tracking-commodity-prices-ledger/prices.py
+++ 2020/tracking-commodity-prices-ledger/prices.py
@@ -1,17 +1,19 @@
+#!/usr/bin/env python3
+
 from datetime import datetime, timedelta
 import json
 import os
 import requests
 import sys
 
-SYMBOLS = ['VTSAX']
-API_KEY = 'YOUR_API_KEY_HERE'  # get your API key at https://www.alphavantage.co/support/#api-key
+SYMBOLS = ['VTSAX']  # add more symbols here
+API_KEY = 'YOUR_API_KEY_HERE'  # get your API key at https://api.tiingo.com
 PRICES_FILE = '/path/to/prices.db'
 
 def query_data(symbol):
     # Pull price data
-    url = 'https://www.alphavantage.co/query'
-    payload = {'function': 'GLOBAL_QUOTE', 'symbol': symbol, 'apikey': API_KEY}
+    url = f'https://api.tiingo.com/tiingo/daily/{symbol}/prices'
+    payload = {'token': API_KEY}
     r = requests.get(url, params=payload)
     return r.json()
 
@@ -19,33 +21,38 @@ def query_data(symbol):
 def output(symbol, prices):
     data = query_data(symbol)
 
-    if 'Global Quote' not in data:
+    # Failure response is a string. Data response is an array.
+    if not isinstance(data, list):
         print('no data found, exiting (probably got rate limited)...')
         sys.exit(0)
 
-    data = data['Global Quote']
-    symbol = data['01. symbol']
-    current_price = data['05. price']
-    date = data['07. latest trading day']
-    if not symbol or not current_price or not date:
-        print('correct data not found, exiting...')
+    data = data[0]
+
+    adjusted_close = data['adjClose']
+    date = data['date']
+    if not adjusted_close or not date:
+        print('data not found in response, exiting...')
         sys.exit(0)
 
-    date = datetime.strptime(date, '%Y-%m-%d').date()
+    # Convert date string to date object.
+    date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z').date()
 
+    # Look through existing entries to make sure a price for this date doesn't
+    # already exist.
     for price in prices:
         if len(price) != 4:
             continue
 
+        # Grab date and symbol from existing entry.
         parsed_date = datetime.strptime(price[1], '%Y/%m/%d').date()
         parsed_symbol = price[2]
 
         if parsed_symbol == symbol and parsed_date == date:
-            print(f'existing price found for {symbol} on {date}: {price[3]} (${current_price})')
+            print(f'existing price found for {symbol} on {date}: {price[3]} (${adjusted_close})')
             return None
 
     date = date.strftime('%Y/%m/%d')
-    output = f'P {date} {symbol} ${current_price}\n'
+    output = f'P {date} {symbol} ${adjusted_close}\n'
     return output
 
 
